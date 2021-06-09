265413de8708fcc0a5bfcc4740b36a772744ce22
sunflower-exe <49395120+sunflower-exe@users.noreply.github.com>
2021-06-09 07:50:11 +0100

    Update starscan.py
    Nmap integration in the works.

--- starscan.py
+++ starscan.py
@@ -1,6 +1,6 @@
 # Add a text to write to
 # Set up threading +
-# Add options to scan individual ports
+# Add options to scan individual ports +
 # Nmap intergration
 # Set up arguments
 
@@ -12,6 +12,7 @@ import sys
 from datetime import datetime
 import time
 import platform
+import os
 
 if platform.system() == "Windows":
     subprocess.call("cls", shell=True)
@@ -25,56 +26,89 @@ print(
 ███████╗   ██║   ███████║██████╔╝███████╗██║     ███████║██╔██╗ ██║            
 ╚════██║   ██║   ██╔══██║██╔══██╗╚════██║██║     ██╔══██║██║╚██╗██║            
 ███████║   ██║   ██║  ██║██║  ██║███████║╚██████╗██║  ██║██║ ╚████║            
-╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝ Version 0.2
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝ Version 0.3
 """
 )
 time.sleep(0.5)
 
-remoteServer = input("Enter a remote host to scan: ")
-remoteServerIP = socket.gethostbyname(remoteServer)
-portRange = input("Enter port range: ")
-
-print("-" * 67)
-print(f"[+] Scanning Target: {remoteServer}")
-print(f"[+] Scanning started at: {str(datetime.now())}")
-print("-" * 67)
-
-t1 = datetime.now()
-
-
-def main(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
     try:
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print(f"[+] Port {port}     Open")
-        sock.close()
-    except KeyboardInterrupt:
-        print("[!] Exiting...")
-    except socket.gaierror:
-        print("[!] Hostname could not be resolved. Exiting...")
-        sys.exit()
+        remoteServer = input("Enter a remote host to scan: ")
+        remoteServerIP = socket.gethostbyname(remoteServer)
+        portRange = input("Enter port range: ")
+    except socket.gaierror as e:
+        print('Host name could not be resolved!')
+        main()
+    discoveredPorts = []
 
-def threader():
-    while True:
-        worker = q.get()
-        main(worker)
-        q.task_done()
+    print("-" * 67)
+    print(f"[+] Scanning Target: {remoteServer}")
+    print(f"[+] Scanning started at: {str(datetime.now())}")
+    print("-" * 67)
 
-q = Queue()
+    t1 = datetime.now()
 
-for i in range(10000):
-    t = threading.Thread(target=threader, daemon=True)
-    t.start()
 
-for worker in range(1, int(portRange)):
-    q.put(worker)
+    def scanner(port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print(f"[+] Port {port}     Open")
+                discoveredPorts.append(port)
+            sock.close()
+        except KeyboardInterrupt:
+            print("[!] Exiting...")
+        except socket.gaierror:
+            print("[!] Hostname could not be resolved. Exiting...")
+            sys.exit()
 
-q.join()
+    def threader():
+        while True:
+            worker = q.get()
+            scanner(worker)
+            q.task_done()
 
-t2 = datetime.now()
-totalScanTime = t2 - t1
-print(f"\nThe scan took {totalScanTime}.")
+    q = Queue()
+
+    for i in range(200):
+        t = threading.Thread(target=threader, daemon=True)
+        t.start()
+
+    #for worker in range(1, int(portRange)):
+        #q.put(worker)
+
+    for worker in range(int(portRange)):
+        q.put(worker)
+
+    q.join()
+
+    time.sleep(0.5)
+    print("-" * 67)
+    print(f'[+] Discovered Ports:')
+    for port in discoveredPorts:
+        print(f' - {port}')
+    t2 = datetime.now()
+    totalScanTime = t2 - t1
+    print(f"\nThe scan took {totalScanTime}.")
+
+    # Nmap
+
+    #def nmap():
+        #try:
+            #nmapScan = input(f'Would you like to run a suggested scan on ports {discoveredPorts} (y/n): ')
+            #if nmapScan == 'y':
+                #nmapFile = 'nmap -p{port} -sC -sV -T4 {ip}'.format(port=','.join(discoveredPorts), ip=remoteServerIP)
+                #print(nmapFile)
+                #os.mkdir(remoteServerIP)
+                #os.chdir(remoteServerIP)
+                #os.system(nmapFile)
+        #except FileExistsError as e:
+           #print(e)
+            #exit()
+    #nmap()
+
+
 
 if __name__ == "__main__":
-    main(65535)
+    main()
