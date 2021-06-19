abbbd398711d3ac50c559a22ee78209ae53194a8
sunflower-exe <49395120+sunflower-exe@users.noreply.github.com>
2021-06-19 15:05:51 +0100

    Release 1.0
    This release came with nmap integration. Now after the port scan a user will be asked if they want to proceed with the nmap scan.

--- starscan.py
+++ starscan.py
@@ -20,12 +20,13 @@ print(
 ███████╗   ██║   ███████║██████╔╝███████╗██║     ███████║██╔██╗ ██║            
 ╚════██║   ██║   ██╔══██║██╔══██╗╚════██║██║     ██╔══██║██║╚██╗██║            
 ███████║   ██║   ██║  ██║██║  ██║███████║╚██████╗██║  ██║██║ ╚████║            
-╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝ Version 0.4
+╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝ Version 1.0
 """
 )
 time.sleep(0.5)
 
 def main():
+    discoveredPorts = []
     try:
         remoteServer = input("Enter a remote host to scan: ")
         remoteServerIP = socket.gethostbyname(remoteServer)
@@ -33,15 +34,12 @@ def main():
     except socket.gaierror as e:
         print('Host name could not be resolved!')
         main()
-    discoveredPorts = []
-
-    print("-" * 67)
-    print(f"[+] Scanning Target: {remoteServer}")
-    print(f"[+] Scanning started at: {str(datetime.now())}")
-    print("-" * 67)
 
     t1 = datetime.now()
-
+    print("-" * 67)
+    print(f"[+] Scanning Target: {remoteServer}")
+    print(f"[+] Scanning started at: {t1}")
+    print("-" * 67)
 
     def scanner(port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,30 +47,24 @@ def main():
             result = sock.connect_ex((remoteServerIP, port))
             if result == 0:
                 print(f"[+] Port {port}     Open")
-                discoveredPorts.append(port)
+                discoveredPorts.append(str(port))
             sock.close()
-        except KeyboardInterrupt:
-            print("[!] Exiting...")
         except socket.gaierror:
             print("[!] Hostname could not be resolved. Exiting...")
             sys.exit()
 
     def threader():
-        while True:
-            worker = q.get()
-            scanner(worker)
-            q.task_done()
+        worker = q.get()
+        scanner(worker)
+        q.task_done()
 
     q = Queue()
 
-    for i in range(500):
+    for i in range(225):
         t = threading.Thread(target=threader, daemon=True)
         t.start()
 
-    # for worker in range(1, int(portRange)):
-    #     q.put(worker)
-
-    for worker in range(int(portRange)):
+    for worker in range(0, int(portRange)):
         q.put(worker)
 
     q.join()
@@ -87,27 +79,27 @@ def main():
     print(f"\nThe scan took {totalScanTime}.")
 
     # Nmap
+    if platform.system() == "Windows":
+        quit()
 
-    # def nmap():
-    #     try:
-    #         nmapScan = input(f'Would you like to run a suggested scan on ports {discoveredPorts} (y/n): ')
-    #         if nmapScan == 'y':
-    #             cmd = 'nmap -p{port} -sC -sV -T4 {ip}'.format(port=','.join(discoveredPorts), ip=remoteServerIP)
-    #             print(cmd)
-    #             #Make a directory to write the found ports in NMAP
-    #             #os.mkdir(f'{remoteServerIP} / STARSCAN')
-    #             #os.chdir(remoteServerIP)
-    #             os.system(cmd)
-    #     except FileExistsError as e:
-    #         print(e)
-    #         exit()
-    #     except Exception as noPorts:
-    #         print('[!] No ports specified!', noPorts)
-    # nmap()
+    def nmap():
+        nmapScan = input(f'Would you like to run a suggested scan on ports {discoveredPorts} (y/n): ')
+        if nmapScan == 'y':
+                try:
+                    cmd = 'nmap -p{port} -sC -sV -T4 {ip}'.format(port=','.join(discoveredPorts), ip=remoteServerIP)
+                    print(cmd)
+                    # Make a directory to write the found ports in NMAP
+                    # os.mkdir(f'{remoteServerIP} / STARSCAN')
+                    # os.chdir(remoteServerIP)
+                    os.system(cmd)
+                except Exception as e:
+                    print(e)
+                    exit()
+    nmap()
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('[!] Exiting...')
+        print('\n[!] Exiting...')
         quit()
